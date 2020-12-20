cf42782dffe6a08cc83c2f8e959f41e6c9cb0d17
Tatiana <tatianaw83@gmail.com>
2020-12-20 13:58:52 -0800

    initialized client side of Vue app with TailwindCSS

---
+++ client/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="flex flex-row justify-between">
+    <h1 class="text-2xl">Note.ably - Vue</h1>
+    <nav class="w-1/3 flex flex-row justify-between">
+      <a href="/login">Log In</a>
+      <a href="/signup">Sign Up</a>
+      <a href="/logout">Log Out</a>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style>
+
+</style>
